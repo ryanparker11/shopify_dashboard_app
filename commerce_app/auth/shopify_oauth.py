ed02228278882042db31c7a0f7a989c936e981fd
Ryan Parker <ryanparker1107@gmail.com>
2025-10-22 12:00:58 -0400

    adding UI

--- commerce_app/auth/shopify_oauth.py
+++ commerce_app/auth/shopify_oauth.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
-load_dotenv()
+
 
 router = APIRouter(prefix="/auth", tags=["shopify-auth"])
 
